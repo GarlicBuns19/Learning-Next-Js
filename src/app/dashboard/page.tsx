a490860536db61fd3f12cf9ca15e100900ef1774
GarlicBuns19 <planterx26@gmail.com>
2023-06-09 18:53:05 +0200

    First Commit
    feat: App Dashboard Works

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,6 @@
+// `app/dashboard/page.tsx` is the UI for the `/dashboard` URL
+export default function Dashboard() {
+  return (
+    <h2 className={"text-white font-bold text-2xl"}>App Dashboard</h2>
+  )
+}
